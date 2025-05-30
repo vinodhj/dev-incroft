4d1ac8b1148cec963a4914788c577f60841fee95
Vinodh <vinodh.jeevanantham@gmail.com>
2025-05-30 18:15:41 +0530

    removed hom ver2,3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import './App.css';
-import ComingSoonLanding from './home-design/coming-soon';
+// import ComingSoonLanding from './home-design/coming-soon';
 import DeityHomepage from './home-design/ver1';
-import UranusHomepage from './home-design/ver2';
-import BetterStackLanding from './home-design/ver3';
 
 function App() {
-  // return <UranusHomepage />;
-  // return <BetterStackLanding />;
   return <DeityHomepage />;
   // return <ComingSoonLanding />;
 }
